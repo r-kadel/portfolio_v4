83eb3a7599c8952ff91fff98cb58d7bfbf6a4954
Ryan Kadel <ryan.kadel88@gmail.com>
2021-02-26 20:02:45 -0500

    first draft

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react"
-import logo from "../assets/logo.svg"
+// import logo from "../assets/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
 const Navbar = ({toggleSidebar}) => {
   return <nav className="navbar">
     <div className="nav-center">
       <div className="nav-header">
-        <img src={logo} alt="logo" />
+        <span className="logo">Logo Here?</span>
         <button type="button" className="toggle-btn" onClick={toggleSidebar}>
           <FaAlignRight />
         </button>
